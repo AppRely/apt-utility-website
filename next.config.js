82f757f4ea38c2c42f3cb2a5ebb9d1a35de39c77
BhartiPawar-apprely <bharti_pawar@apprely.com>
2025-08-22 11:12:59 +0530

    add konva and update react version

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
     serverActions: true,
     turbopack: true,
   },
+  // Disable canvas module to run on server 
+  webpack: (config) => {
+    config.resolve.alias.canvas = false; 
+    return config;
+  },
 };
 
 module.exports = nextConfig;
