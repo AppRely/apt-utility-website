d1b9e237cc65c4d0e8f4cd5197d3133a1951407d
BhartiPawar-apprely <bharti_pawar@apprely.com>
2025-09-02 17:42:14 +0530

    added konva temporary

---
+++ src/components/dashboard/KonvaWrapper.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import dynamic from "next/dynamic";
+
+// ✅ Import your Konva component dynamically, client-only
+const Konva = dynamic(() => import("@/components/dashboard/Konva"), {
+  ssr: false, // disable SSR
+  loading: () => <p>Loading canvas...</p>,
+});
+
+export default function KonvaWrapper() {
+  return <Konva />;
+}
