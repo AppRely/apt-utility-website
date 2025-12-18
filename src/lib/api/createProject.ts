4b3516985655ee7eef64838c50f2af2bc12ff8a6
vaibhavgobre <vaibhav_gobre@apprely.com>
2025-12-18 17:30:59 +0530

    fix: create project fix

--- src/lib/api/createProject.ts
+++ src/lib/api/createProject.ts
@@ -1,12 +1,13 @@
 const createProject = async (formData: FormData) => {
   const API_BASE = `${process.env.NEXT_PUBLIC_SERVER_ENDPOINT}`;
-  const response = await fetch(`${API_BASE}/api/v1/videos/project-upload`, {
+  const response = await fetch(`${API_BASE}/api/v1/videos/project-upload/`, {
     method: "POST",
     body: formData,
   })
   console.log("Response:", response)
   if (!response.ok) {
-    throw new Error("Failed to create project")
+    const errorData = await response.json().catch(() => null);
+    throw new Error(errorData?.message || JSON.stringify(errorData) || "Failed to create project");
   }
   return response.json()
 }
