f32380671009031ec3089ee95eadd3d9490633cf
vaibhavgobre <vaibhav_gobre@apprely.com>
2025-12-26 18:47:55 +0530

    fix: get activity logs

--- src/lib/api/getActivityLogs.ts
+++ src/lib/api/getActivityLogs.ts
@@ -1,7 +1,7 @@
 const API_BASE = process.env.NEXT_PUBLIC_SERVER_ENDPOINT;
 
-export async function getActivityLogs(videoId: number) {
-  const res = await fetch(`${API_BASE}/api/v1/videos/activity/logs/?video_id=${videoId}`);
+export async function getActivityLogs(projectId: number) {
+  const res = await fetch(`${API_BASE}/api/v1/videos/activity/logs/?project_id=${projectId}`);
 
   if (!res.ok) {
     throw new Error("Failed to fetch activity logs");
