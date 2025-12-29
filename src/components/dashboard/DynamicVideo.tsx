aa0251bdb08531be13fd40b16b9d58f8129b7621
BhartiPawar-apprely <bharti-pawar@apprely.com>
2025-12-29 13:20:54 +0530

    fix: update videoId to projectId for consistency in DynamicVideo and modify formData keys in Sidebar

--- src/components/dashboard/DynamicVideo.tsx
+++ src/components/dashboard/DynamicVideo.tsx
@@ -495,12 +495,12 @@ export default function DynamicVideo({
     };
   }, [video, fps]);
 
-
+  const projectId = Number(typeof window !== "undefined" ? sessionStorage.getItem("projectId") : null);
   const chunkMutation = useMutation({
     mutationFn: async ({ start, end }: { start: number; end: number }) => {
-      if (!videoId) return Promise.resolve(null);
+      if (!projectId) return Promise.resolve(null);
 
-      return getFrameRangeData(videoId, start, end);
+      return getFrameRangeData(projectId, start, end);
     },
 
     onSuccess: (data, variables) => {
@@ -575,7 +575,6 @@ export default function DynamicVideo({
     },
   });
 
-  const projectId = Number(typeof window !== "undefined" ? sessionStorage.getItem("projectId") : null);
   const objectMutation = useMutation({
     mutationFn: ({
       projectId,
