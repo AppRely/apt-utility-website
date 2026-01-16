fc1adb3dd3db9cc0caf2e546ec015178f98f40bf
vaibhavgobre <vaibhav_gobre@apprely.com>
2026-01-16 18:13:44 +0530

    fix: showing 50 frame trajectory from current

--- src/components/dashboard/DynamicVideo.tsx
+++ src/components/dashboard/DynamicVideo.tsx
@@ -1392,8 +1392,9 @@ export default function DynamicVideo({
                 allObjectIds.map((objectId) => {
                   const points = getTrajectoryPointsUpToCurrent(
                     objectId,
-                    currentFrame
+                    currentFrame+50
                   );
+
                   if (points.length < 2) return null;
 
                   return (
